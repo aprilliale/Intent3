5745a1db8c640334b6b2620ead6dfee77d18ba57
Erron <eron_lolha@yahoo.com>
2016-10-11 09:15:32 +0700

    Intent SMS

--- app/src/main/java/id/sch/smktelkom_mlg/learn/intent3/MainActivity.java
+++ app/src/main/java/id/sch/smktelkom_mlg/learn/intent3/MainActivity.java
@@ -19,11 +19,26 @@ public class MainActivity extends AppCompatActivity {
                 dialPhoneNumber("087876748181");
             }
         });
+
+        findViewById(R.id.imageViewSms).setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                composeSmsMessage("Pesan dari SMK Telkom Malang");
+            }
+        });
     }
 
     public void dialPhoneNumber(String phoneNumber) {
         Intent intent = new Intent(Intent.ACTION_DIAL);
         intent.setData(Uri.parse("tel:" + phoneNumber));
+        if (intent.resolveActivity(getPackageManager()) != null)
+            startActivity(intent);
+    }
+
+    public void composeSmsMessage(String message) {
+        Intent intent = new Intent(Intent.ACTION_SEND);
+        intent.setType("text/plain");
+        intent.putExtra("sms_body", message);
         if (intent.resolveActivity(getPackageManager()) != null)
             startActivity(intent);
     }
